bcf5cfa704054caa774c605dcb27d2fe123719a3
Stéphane L <Palakis@users.noreply.github.com>
2017-05-09 09:29:56 +0200

    Update fortilogin.py

--- fortilogin.py
+++ fortilogin.py
@@ -1,5 +1,5 @@
 #    fortilogin.py
-#    Copyright (C) 2015 Stéphane Lepin
+#    Copyright (C) 2015 Stephane Lepin
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
